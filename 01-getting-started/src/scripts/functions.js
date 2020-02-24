36a4a13800ab3baa4f4a62b1ae1718d7cf6be44d
David Robertson <Foinaven@yahoo.com>
2020-02-24 13:19:52 -0700

    round 1 changes

--- 01-getting-started/src/scripts/functions.js
+++ 01-getting-started/src/scripts/functions.js
@@ -4,6 +4,7 @@ const functions = {
     size: (num) => {
         if (num < 10) return "small";
         if (num < 20) return "medium";
+        if (num = -101) return "extra-large";
         return "large";
     },
 
@@ -13,9 +14,13 @@ const functions = {
 
     subtract: (num1, num2) => {
         return num1 - num2;
-    }
+    },
 
-    
+    isEven: (num1) => {
+        var x = num1 % 2;
+        if (x = 0);
+            return true;
+    },
 };
 
 export default functions;
