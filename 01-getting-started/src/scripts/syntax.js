62c3bc742edd688310a1133d2a0f48ef98709098
David Robertson <Foinaven@yahoo.com>
2020-02-27 09:58:21 -0700

    27 Feb edits

--- 01-getting-started/src/scripts/syntax.js
+++ 01-getting-started/src/scripts/syntax.js
@@ -9,23 +9,76 @@ const functions={
         else return false;
 },
 
- defineString:(num) =>{
+//string
+ defineString:(num) =>  {
      const x = typeof(num);
        return (x);
  },
 
-};
-/*string
-boolean
-array
-dictionary / objects
-undefined
-sample if / else
-functions
-parameters
-returns
-arrays
-add to the front
+ //boolean
+
+ defineBoolean:(num) =>  {
+      const x = typeof(num);
+        return (x);
+  },
+
+//array
+  isVehicle:() => {
+  var cars = ["BMW", "Volvo", "Ford"];
+  var motorbikes = ["Ducatti", "Yahama", "Suzuki"];
+  var vehicles = cars.concat(motorbikes);
+  return vehicles.length;
+  },
+
+//dictionary / objects
+  myPlane:(x) => {
+  var plane = {planeType: "Boeing", planeModel: 787, planeColor: "white"};
+    return plane[x];
+  },
+
+
+//sample if/else class
+//Note use of brackets and braces in if/else sections.
+  idCheck:(person) => {
+    if (person == "Bob") {
+      return "Hello Bob!";
+     } else {return "I don't know you,";
+  }
+  },
+
+//undefined
+
+isDefined:(input) => {
+    if (input == undefined) {
+      return "x is undefined";
+      }  else {return "x is defined";
+}
+},
+
+//functions
+mySum:(a,b,c) => {
+  return a + b + c; // function returns sum of 3 values
+},
+
+
+//returns
+
+myReturn:(num) => {
+  var x = (Number(num) + 1);
+    return x;
+},
+
+//arrays
+//Add to the front
+
+addFront:(name) => {
+var x = ["Fred", "Peggy", "James"];
+x.unshift("Bob");
+return x;
+}
+
+
+/*add to the front
 add to the end
 update values
 loops 
@@ -38,4 +91,5 @@ Objects / Dictionaries
 declare object
 lookup key to retrieve value
 */
+}
 export default functions;
--- 01-getting-started/src/scripts/syntax.test.js
+++ 01-getting-started/src/scripts/syntax.test.js
@@ -1,4 +1,4 @@
-import functions from './syntax'
+import functions from './syntax.js'
 
 //Test if input character is a number
 
@@ -14,4 +14,50 @@ input type*/
 test('Does defineString work?', () => {
     expect(functions.defineString("dingo")).toBe("string");
     
+});
+
+//Boolean test
+
+test('Does defineBoolean work?', () => {
+    expect(functions.defineBoolean(true)).toBe("boolean");
+});
+
+//Array test
+
+test('Does isVehicle work?' , () => {
+    expect(functions.isVehicle()).toBe(6);
+});
+
+//dictionary / objects test
+//Go over this in detail and break it down. Lots of lessons here.
+test('Does plane work?', () => {
+    expect(functions.myPlane("planeType")).toBe("Boeing");
+});
+
+//else / if class test
+
+test('Does idCheck work?' , () => {
+    expect(functions.idCheck("Bob")).toBe("Hello Bob!");
+});
+
+//undefined test
+
+test('Does isDefined work?', () => {
+    expect(functions.isDefined("Sparps")).toBe("x is defined"); 
+});
+
+//functions test
+
+test('Does mySum work?' , () => {
+    expect(functions.mySum(2,3,5)).toBe(10);
+});
+
+//returns test
+test('Does myReturn work?', () => {
+    expect(functions.myReturn(8)).toBe(9);
+});
+
+//unshift array  test
+test('Does addFront work?' , () => {
+    expect(functions.addFront()).toStrictEqual(["Bob", "Fred", "Peggy", "James"]);
 });
